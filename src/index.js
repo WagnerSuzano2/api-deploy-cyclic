225d08f7f6613fa6ab138ccd993cca318600570b
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2023-11-20 19:03:57 -0300

    deploy nome

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.get('/', async (req, res) => {
-    return res.json('Api está Ok!')
+    return res.json('Api está Ok! Wagner')
 })
 const port = process.env.PORT || 3005
 
