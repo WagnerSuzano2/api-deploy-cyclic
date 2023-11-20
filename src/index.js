464634862ebeea153a7be6288ce3a9e4190f85ce
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2023-11-20 18:34:34 -0300

    deploy inicial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+//e usado esta maneira caso o arquivo .env não esteja na raiz do projeto
+//require('dotenv').config({path:'.src/.env'})
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.get('/', async (req, res) => {
+    return res.json('Api está Ok!')
+})
+const port = process.env.PORT || 3005
+
+app.listen(port, () => {
+    console.log(`Servidor em pé na porta ${port}`)
+})
