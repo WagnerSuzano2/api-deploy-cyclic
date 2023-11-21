5b9754dc55e6a910483657cb3d88575c06adf5e6
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2023-11-20 21:26:05 -0300

    deploy banco usuario

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const knex = require('./conexao');
 //e usado esta maneira caso o arquivo .env não esteja na raiz do projeto
 //require('dotenv').config({path:'.src/.env'})
 const express = require('express');
@@ -6,8 +7,13 @@ const app = express();
 
 app.use(express.json());
 app.get('/', async (req, res) => {
-    return res.json('Api está Ok! Wagner s')
-})
+    try {
+        const listarUsuarios = await knex('usuarios').debug();
+        return res.json(listarUsuarios);
+    } catch (error) {
+        return res.status(500).json({ mensagem: "Erro Interno do Servidor" });
+    }
+});
 const port = process.env.PORT || 3005
 
 app.listen(port, () => {
