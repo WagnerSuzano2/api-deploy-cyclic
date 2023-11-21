1a85c2178a6e3e15c17c2efcb6c3824861e2af6d
Wagner Suzano Urivi <wagnersuzano20@gmail.com>
2023-11-20 23:45:05 -0300

    configuração de cors

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const knex = require('./conexao');
 //e usado esta maneira caso o arquivo .env não esteja na raiz do projeto
 //require('dotenv').config({path:'.src/.env'})
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
